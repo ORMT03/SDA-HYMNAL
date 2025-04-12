37304a596ac747653d1b8fb57b6fde2184a18a3d
ORMT03 <124946139+ORMT03@users.noreply.github.com>
2025-04-12 13:05:04 -0500

    Updated web version to 11.0

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const MAJOR_VERSION = '10';  // Change this for major version updates
+const MAJOR_VERSION = '11';  // Change this for major version updates
 const MINOR_VERSION = '0';  // Change this for minor version updates
 
 const MAJOR_CACHE = `pwa-cache-major-v${MAJOR_VERSION}`;

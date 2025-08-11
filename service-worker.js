66d005a95aa6a854720372c1f2795d255e6372fc
ORMT03 <124946139+ORMT03@users.noreply.github.com>
2025-08-11 18:40:38 -0500

    Version 13.0 - Fixed Lyrics and Sheet Music for songs in both English and Spanish
    Fixed lyrics for English songs: 065, 086, 099, 100, 108, 132, 159, 206, 223, 240, 287, 300, 306, 313, 314, 329, 341, 348, 358, 432, 439, 447, 524, 529, 530, 545, 551, 567, 590, 671
    
    Fixed Sheet Music for English 248 & 359, and Spanish 164 & 541

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const MAJOR_VERSION = '12';  // Change this for major version updates
+const MAJOR_VERSION = '13';  // Change this for major version updates
 const MINOR_VERSION = '0';  // Change this for minor version updates
 
 const MAJOR_CACHE = `pwa-cache-major-v${MAJOR_VERSION}`;
